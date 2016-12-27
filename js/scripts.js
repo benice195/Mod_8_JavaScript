25f83e252dec507f1867d2b6098195566329c751
Wojciech Szczyrba <benice195@gmail.com>
2016-12-27 18:45:21 +0100

    Poprawa kodu if, poprawny zapis

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
-var a = '2',
-    b = '4',
-    value = (a*a) + (2 * a * b) - (b*b)
-;
+var a = '2';
+    b = '4';
+    value = (a * a) + (2 * a * b) - (b * b);
+
 console.log('Wynik: '+value+' ');
 
 
 if (value > 0) {
     console.log('wynik dodatni');
-    
 }   else if (value < 0) {
     console.log('wynik ujemny');
-} 
-
-var równyZero = value = 0 ? 'Wynik jest równy zero' : 'Wynik nie jest równy zero';
-console.log(równyZero);
+}   else if (value = 0) {
+    console.log('równy zero');
+}
