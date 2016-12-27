cc8ea9e6e3fe991d377fca3b1a015183d406d68c
Wojciech Szczyrba <benice195@gmail.com>
2016-12-27 16:01:48 +0100

    Funckje JavaScript

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+var triangle1Area = getTriangleArea(4, 11);
+console.log("Powierzchnia 1 trójkąta: ", (triangle1Area) )
+var triangle2Area = getTriangleArea(2, -8);
+console.log("Powierzchnia 2 trójkąta: ", (triangle2Area) )
+var triangle3Area = getTriangleArea(7, 7);
+console.log("Powierzchnia 3 trójkąta: ", (triangle3Area) )
+
+function getTriangleArea(a, h) {
+        
+    if ( (a < 0) || (h < 0) ) 
+            console.log('Nieprawidłowe dane');
+    return (a* h/2)
+        
+}
+
+/*function getTriangleArea(a, h) {
+        
+    if ( (a >= 0) && (h >= 0) ) 
+            console.log('Wartości są dodatnie');
+        
+    else if ( (a < 0) || (h < 0) ) 
+            console.log('Nieprawidłowe dane');
+    return (a* h/2)
+        
+}*/
+
+
+
