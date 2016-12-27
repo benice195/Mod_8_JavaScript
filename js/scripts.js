88407a9fa7e9bcd09279f2c7972a81476e983cc7
Wojciech Szczyrba <benice195@gmail.com>
2016-12-27 11:29:38 +0100

    Dodatek_kodu_JS_alerty

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// plik scripts.js
+var name = prompt('Wpisz twoje imię');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+var color = prompt('Wpisz swój kolor');
+alert('Wybrano, ' + color);
+console.log('Twój kolor to: ' + color);
