eff5e912ce6e778b02abe4f9584cf931a785ab09
Wojciech Szczyrba <benice195@gmail.com>
2016-12-27 13:19:42 +0100

    Dodatkowe opis dodawania

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ var a = '4',
     triangleArea = [a*h/2]
 ;
 console.log('Pole trójkąta o podstawie a: '+ a + ' i wysokości h: ' + h +' wynosi: ' + triangleArea );
+
+var userName = 'Janek Kos',
+    wiek = '12',
+    zawód = 'uczeń',
+    auto = '5',
+    kijek = '2',
+    brat = '1',
+    siostra = '2',
+    rodzeństwo = '3',
+    ilośćZabawek = (auto*1+kijek*1)
+;
+ console.log('Młody '+userName+' ma '+wiek+' lat i obecnie to '+zawód+', ma '+brat+' brata i '+siostra+' siostry. Razem uzbierali '+ ilośćZabawek +' rzeczy z czego '+kijek+' kijki i '+auto+' zabawkowych aut.')
