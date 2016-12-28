fd5c51f6f9dac59d057bfca5c6346eb3c07b8e36
Wojciech Szczyrba <benice195@gmail.com>
2016-12-28 14:40:31 +0100

    Dodano ciagi zadanie Papugi i Slonie

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var textBasic = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var textAnimal = 'Zielone słonie';
+var textAnimalUpperCased = textAnimal.toUpperCase();
+console.log(textAnimalUpperCased);
+
+var textBasic = textBasic.replace('Papugi', textAnimalUpperCased);
+console.log(textBasic);
+console.log(textBasic.length/2);
+console.log(textBasic.length);
+console.log(textBasic.slice(0, 45));
