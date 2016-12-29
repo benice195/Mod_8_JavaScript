9b4245faee70b8579664582b2fbdaed0afabd60f
Wojciech Szczyrba <benice195@gmail.com>
2016-12-29 14:45:42 +0100

    zamiana else if na if

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var a = '2';
+var a = '-2';
     b = '4';
     value = (a * a) + (2 * a * b) - (b * b);
 
@@ -9,6 +9,6 @@ if (value > 0) {
     console.log('wynik dodatni');
 }   else if (value < 0) {
     console.log('wynik ujemny');
-}   else (value === 0) {
+}   if (value === 0) {
     console.log('równy zero');
 }
